51033b67b94be13bb7550866f05b55691ad8984e
Koji Inoue <inouekoji2013@gmail.com>
2020-12-18 09:12:30 +0900

    Add files via upload

--- chroma.py
+++ chroma.py
@@ -12,7 +12,14 @@ import numpy
 def hz2nn(frequency):
 	return int (round (12.0 * (math.log(frequency / 440.0) / math.log (2.0)))) + 69
 
-# スペクトルと対応する周波数ビンの上を受け取り，クロマベクトルを算出
+#
+# スペクトルと対応する周波数ビンの情報を受け取り，クロマベクトルを算出
+#
+# 例えば，サンプリング周波数が16000の場合は，spectrumは8000Hzまでの情報を保持していることになるため，
+# フレームサイズが512だとすると，
+# frequencies = [1 * (8000/512), 2 * (8000/512), ..., 511 * (8000/512), 8000] とすればよい
+# このような処理をnumpyで実現するばらば，
+# frequencies = np.linspace(1/len(spectrum), 8000, len(spectrum)) などどすればよい
 def chroma_vector(spectrum, frequencies):
 	
 	# 0 = C, 1 = C#, 2 = D, ..., 11 = B
